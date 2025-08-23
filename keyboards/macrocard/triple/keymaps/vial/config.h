6c3b52d5d4f5a886b46182c1b4b2e75f3569e809
mins <insley.matthew@gmail.com>
2025-08-23 00:58:37 -0700

    add macrocards qtpy_debug and triple

---
+++ keyboards/macrocard/triple/keymaps/vial/config.h
@@ -0,0 +1,7 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+
+#pragma once
+
+#define VIAL_KEYBOARD_UID {0xDA, 0xE5, 0x0C, 0x81, 0x94, 0x0B, 0x3D, 0xD7}
+#define VIAL_UNLOCK_COMBO_ROWS { 0, 0 }
+#define VIAL_UNLOCK_COMBO_COLS { 0, 2 }
